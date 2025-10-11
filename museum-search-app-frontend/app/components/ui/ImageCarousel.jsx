4fdd279399a5bdf06e43f597b577b41683e52773
古宮隆宏 <tennis10sao03@outlook.jp>
2025-10-11 22:16:54 +0900

    フェッチをISRに変更

--- museum-search-app-frontend/app/components/ui/ImageCarousel.jsx
+++ museum-search-app-frontend/app/components/ui/ImageCarousel.jsx
@@ -1,34 +1,18 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Navigation, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { useEffect, useState } from 'react';
-import { useAuth } from '@/app/context/AuthContext';
 
-export default function ImageCarousel() {
-  const [museums, setMuseums] = useState([]);
-  const { token } = useAuth();
-  
-  useEffect(() => {
-    const fetchMuseums = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/museums/all`, {
-        headers: {
-          "Authorization": `Bearer ${token}`
-        },
-      });
-      if (!res.ok) {
-        throw new Error('美術館の取得に失敗しました');
-      }
-      const data = await res.json();
-      setMuseums(data);
-    }
-
-    fetchMuseums();
-  }, []);
+export default async function ImageCarousel() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/museums/all`, {
+    next: {revalidate: 3600 * 24},
+  });
+  if (!res.ok) {
+    throw new Error('美術館の取得に失敗しました');
+  }
+  const museums = await res.json();
 
   return (
     <>
