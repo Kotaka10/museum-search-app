9c86876f9925c9c4e01f3f215bcf388382270c85
古宮隆宏 <tennis10sao03@outlook.jp>
2025-10-26 12:09:40 +0900

    バリデーションの付与/削除

--- museum-search-app-backend/backend/src/main/java/com/example/museumsearch/dto/UserDTO.java
+++ museum-search-app-backend/backend/src/main/java/com/example/museumsearch/dto/UserDTO.java
@@ -3,11 +3,9 @@ package com.example.museumsearch.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class UserDTO {
     private Long id;
 
@@ -21,4 +19,18 @@ public class UserDTO {
     
     @Size(max = 50, message = "ユーザー名は50文字以内で入力してください")
     private String userName;
+
+    public UserDTO() {}
+
+    public UserDTO(Long id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public UserDTO(Long id, String email, String password, String userName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+    }
 }
